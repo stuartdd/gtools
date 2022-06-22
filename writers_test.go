b2f523a51cb19902c217697c39950be115f13261
stuartdd <stuart.d.davies@btinternet.com>
2022-06-22 21:16:28 +0100

    Fix close clash

--- writers_test.go
+++ writers_test.go
@@ -14,51 +14,52 @@ var (
 
 func TestEncryptWriter(t *testing.T) {
 	fw := NewWriter("encrypt:test001", "", stdOut, stdErr)
-	write(t, fw, "zzz", 3)
-	cast(t, fw, "zzz")
-	close(t, fw)
+	writeStuff(t, fw, "zzz", 3)
+	castWriter(t, fw, "zzz")
+	closeWriter(t, fw)
 }
+
 func TestMemoryWriter(t *testing.T) {
 	fw := NewWriter("memory:test001", "", stdOut, stdErr)
-	write(t, fw, "zzz", 3)
-	cast(t, fw, "zzz")
-	write(t, fw, "11", 2)
-	cast(t, fw, "zzz11")
-	write(t, fw, "\ntt", 3)
-	cast(t, fw, "zzz11\ntt")
+	writeStuff(t, fw, "zzz", 3)
+	castWriter(t, fw, "zzz")
+	writeStuff(t, fw, "11", 2)
+	castWriter(t, fw, "zzz11")
+	writeStuff(t, fw, "\ntt", 3)
+	castWriter(t, fw, "zzz11\ntt")
 	m1 := ReadFromMemory("test001")
 	if m1 == nil {
 		t.Fatalf("Error: Could fine test001 in memory")
 	}
-	cast(t, m1, "zzz11\ntt")
+	castWriter(t, m1, "zzz11\ntt")
 	s := MutateStringFromMemCache("[%{abc}], [%{test001}]")
 	if s != "[%{abc}], [zzz11\ntt]" {
 		t.Fatalf("Error: Substitution failed %s expectd [%%{abc}], [zzz11\ntt]", s)
 	}
-	close(t, m1)
+	closeWriter(t, m1)
 }
 
 func TestFileWriter(t *testing.T) {
 	fw1 := NewWriter("test001.txt", "", stdOut, stdErr)
 	defer delete(t, "test001.txt")
-	write(t, fw1, "zzz", 3)
-	read(t, "test001.txt", "zzz")
-	write(t, fw1, "yyy", 3)
-	read(t, "test001.txt", "zzzyyy")
-	close(t, fw1)
-	read(t, "test001.txt", "zzzyyy")
+	writeStuff(t, fw1, "zzz", 3)
+	readFileExp(t, "test001.txt", "zzz")
+	writeStuff(t, fw1, "yyy", 3)
+	readFileExp(t, "test001.txt", "zzzyyy")
+	closeWriter(t, fw1)
+	readFileExp(t, "test001.txt", "zzzyyy")
 	fw2 := NewWriter("test001.txt", "", stdOut, stdErr)
-	write(t, fw2, "zzz", 3)
-	read(t, "test001.txt", "zzz")
-	write(t, fw2, "yyy", 3)
-	read(t, "test001.txt", "zzzyyy")
-	close(t, fw2)
+	writeStuff(t, fw2, "zzz", 3)
+	readFileExp(t, "test001.txt", "zzz")
+	writeStuff(t, fw2, "yyy", 3)
+	readFileExp(t, "test001.txt", "zzzyyy")
+	closeWriter(t, fw2)
 	fw3 := NewWriter("append:test001.txt", "", stdOut, stdErr)
-	read(t, "test001.txt", "zzzyyy")
-	write(t, fw3, "xxx", 3)
-	read(t, "test001.txt", "zzzyyyxxx")
-	close(t, fw3)
-	read(t, "test001.txt", "zzzyyyxxx")
+	readFileExp(t, "test001.txt", "zzzyyy")
+	writeStuff(t, fw3, "xxx", 3)
+	readFileExp(t, "test001.txt", "zzzyyyxxx")
+	closeWriter(t, fw3)
+	readFileExp(t, "test001.txt", "zzzyyyxxx")
 }
 
 func delete(t *testing.T, fileName string) {
@@ -68,7 +69,7 @@ func delete(t *testing.T, fileName string) {
 	}
 }
 
-func read(t *testing.T, fileName string, expected string) {
+func readFileExp(t *testing.T, fileName string, expected string) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		t.Fatalf("Error: Could not open file %s", fileName)
@@ -82,7 +83,7 @@ func read(t *testing.T, fileName string, expected string) {
 	}
 }
 
-func cast(t *testing.T, w io.Writer, expected string) *CacheWriter {
+func castWriter(t *testing.T, w io.Writer, expected string) *CacheWriter {
 	cw, ok := w.(*CacheWriter)
 	if !ok {
 		t.Fatalf("Error: Could not cast to CacheWriter")
@@ -93,7 +94,7 @@ func cast(t *testing.T, w io.Writer, expected string) *CacheWriter {
 	return cw
 }
 
-func close(t *testing.T, w io.Writer) {
+func closeWriter(t *testing.T, w io.Writer) {
 	cw, ok := w.(io.Closer)
 	if !ok {
 		t.Fatalf("Error: Could not cast to Closer")
@@ -104,7 +105,7 @@ func close(t *testing.T, w io.Writer) {
 	}
 }
 
-func write(t *testing.T, w io.Writer, stuff string, expLen int) {
+func writeStuff(t *testing.T, w io.Writer, stuff string, expLen int) {
 	l, err := w.Write([]byte(stuff))
 	if err != nil {
 		t.Fatalf("Error: %s", err.Error())
